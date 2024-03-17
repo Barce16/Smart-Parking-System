2196a5debf39ea3523db797619626b1e1a5fd987
RochilleNabong <nabongrochille@gmail.com>
2024-03-17 13:12:30 +0800

    Unfinish

---
+++ model/searchEngine.php
@@ -0,0 +1,39 @@
+<?php
+include('../Event-Driven/config/database.php');
+
+$value = $_POST['search'];// data that connect to function search.js
+
+$sql = "SELECT * FROM register";
+$result = $conn->query($sql); //connected to search.js
+
+if ($result->num_rows > 0) {
+
+    while ($row = $result->fetch_assoc()) {
+        ?>
+        <!-- database -->
+        <tr>
+            <td style="text-align : center;">
+                <?= $row['ID'] ?>
+            </td>
+            <td>
+                <?= $row['LName'] ?>
+            </td>
+        <!-- database -->
+
+        <!-- Buttons -->
+            <td class="d-grid">
+                <button type="button" class="btn btn-sm btn-block
+            btn-link" data-bs-toggle="modal" data-bs-target="#show-details">
+                    show detail</button>
+            </td>
+        <!-- Buttons -->   
+        
+        <!-- database -->
+        </tr>
+        <?php
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
---
+++ reservation.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- link -->
+    <script src="./assets/js/search.js"> </script>
+    <!-- link -->
+
+    <!-- link for Bootstrap -->
+    <link href="./assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <script src="./assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- link for Bootstrap -->
+
+    <!-- backgournd -->
+    <style>
+        body {
+            background-color: #ecf0f1;
+        }
+    </style>
+    <!-- backgournd -->
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <div class="container-fluid">
+            <!-- image -->
+            <a class="navbar-brand" href="javascript:void(0)">
+                <img src="./assets/img/sport-car.png" height="50">
+            </a>
+            <!-- image -->
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <!-- Navigation bars front -->
+            <div class="collapse navbar-collapse" id="mynavbar">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="./index.php">Dashboard</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./registration.php">Registration</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./reservation.php">Reservation</a>
+                    </li>
+                </ul>
+                
+            </div>
+            <!-- Navigation bars buttom-->
+        </div>
+    </nav>
+    <!-- For Text/Typography Front -->
+    <div class="container-fluid mb-5">
+        <p class="h1 mt-2">Reservation</p>
+        <p class="mt-2">Reserve now!!</p>
+        <div class="card mt-2">
+            <div class="card-header" style="text-align : center;"><-----List of Vacant Space-----></div>
+            <div class="card-body"> <strong> </strong>
+
+    <!-- For Text/Typography Buttom-->
+
+</body>
+<!-- jquery -->
+<script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
+    crossorigin="anonymous"></script>
+<!-- jquery -->
+
+</html>
